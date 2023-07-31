a28ae97432a948a5900ed34738b6106abeb32dec
Suswar Sawant <Suswar.Sawant@asl.mum.sst>
2023-07-31 11:14:36 +0530

    Assignment

--- Cell.js
+++ Cell.js
@@ -1,24 +1,12 @@
-// Cell class
 class Cell {
-    constructor() {
-        this.mark = 'Z';
+    constructor(){
+        this.mark='Z'
     }
-
-    isMarked() {
-        return this.mark !== 'Z';
+    isEmpty(){
+        return this.mark=='Z'
     }
-
-    isEmpty() {
-        return this.mark == 'Z';
-    }
-
-    markCell(symbol) {
-        if (this.isEmpty()) {
-            this.mark = symbol;
-            return true;
-        }
-        return false;
+    markCell(symbol){
+        this.mark=symbol
     }
 }
-
-module.exports = Cell;
+module.exports = Cell
